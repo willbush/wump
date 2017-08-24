897d65c25e7512b22cd9899a9a5f3479725ccf25
willbush <will.g.bush@gmail.com>
2017-08-24 01:34:29 -0500

    add DI action provider to make game testable

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::io;
 // rooms (index + 1 == room number). the second dimension is an array of the
 // adjacent rooms. I just hard coded some valid room values here for ease, but
 // there is a formula that could be used to derive instead.
-static MAP: [[usize; 3]; 20] = [
+static MAP: [[RoomNum; 3]; 20] = [
     [2, 5, 8],
     [1, 3, 10],
     [2, 4, 12],
@@ -46,18 +46,31 @@ fn main() {
     }
 }
 
-fn move_player(room_num: usize) -> usize {
+fn can_move(next: RoomNum, current: RoomNum) -> bool {
+    if current > 0 && current <= MAP.len() {
+        let adj_rooms = MAP[current - 1];
+        let adj1 = adj_rooms[0];
+        let adj2 = adj_rooms[1];
+        let adj3 = adj_rooms[2];
+
+        next == adj1 || next == adj2 || next == adj3
+    } else {
+        false
+    }
+}
+
+fn move_player(room_num: RoomNum) -> RoomNum {
     let adj_rooms = MAP[room_num - 1];
-    let room_a = adj_rooms[0];
-    let room_b = adj_rooms[1];
-    let room_c = adj_rooms[2];
+    let adj1 = adj_rooms[0];
+    let adj2 = adj_rooms[1];
+    let adj3 = adj_rooms[2];
     println!("You are in room {}", room_num);
-    println!("Tunnels leads to {} {} {}", room_a, room_b, room_c);
+    println!("Tunnels leads to {} {} {}", adj1, adj2, adj3);
 
     loop {
         let input = read_sanitized_line();
-        match input.parse::<usize>() {
-            Ok(n) => if n == room_a || n == room_b || n == room_c {
+        match input.parse::<RoomNum>() {
+            Ok(n) => if n == adj1 || n == adj2 || n == adj3 {
                 return n;
             } else {
                 println!("Not Possible - Where to?");
@@ -74,4 +87,141 @@ fn read_sanitized_line() -> String {
         .read_line(&mut input)
         .expect("Failed to read line.");
     input.trim().to_uppercase()
+}
+
+type RoomNum = usize;
+
+#[derive(Debug, PartialEq)]
+enum Action {
+    Move(RoomNum),
+    Shoot(RoomNum),
+    Quit,
+    None,
+}
+
+#[derive(Debug, PartialEq)]
+struct GameState {
+    // The game turn starting from 0. Each player turn increments this by one.
+    turn: usize,
+    // the current room number the player is in.
+    player_room: RoomNum,
+}
+
+trait ActionProvider {
+    fn next(&mut self, game_state: &GameState) -> Action;
+}
+
+struct Game {
+    starting_room: RoomNum,
+    action_provider: Box<ActionProvider>,
+}
+
+impl Game {
+    fn new(starting_room: RoomNum, action_provider: Box<ActionProvider>) -> Game {
+        Game {
+            starting_room: starting_room,
+            action_provider: action_provider,
+        }
+    }
+
+    fn run(&mut self) {
+        let mut room_num = self.starting_room;
+
+        let mut turn = 0;
+
+        loop {
+            println!("You are in room {}", room_num);
+            println!("Shoot, Move, or Quit (S, M, Q)");
+
+
+            let game_state = GameState {
+                turn: turn,
+                player_room: room_num,
+            };
+            let action = self.action_provider.next(&game_state);
+            match action {
+                Action::Move(next_room) => if can_move(room_num, next_room) {
+                    room_num = next_room;
+                },
+                Action::Quit => break,
+                _ => continue,
+            }
+            turn += 1;
+        }
+    }
+}
+
+#[cfg(test)]
+mod game_tests {
+    use super::*;
+
+    struct ActionProviderSpy {
+        actions: Vec<Action>,
+        expected_states: Vec<GameState>,
+    }
+
+    impl ActionProviderSpy {
+        fn new(actions: Vec<Action>, exptected_states: Vec<GameState>) -> Self {
+            ActionProviderSpy {
+                actions: actions,
+                expected_states: exptected_states,
+            }
+        }
+    }
+
+    impl ActionProvider for ActionProviderSpy {
+        fn next(&mut self, actual_state: &GameState) -> Action {
+            let expected_state = &self.expected_states[actual_state.turn];
+
+            assert_eq!(*expected_state, *actual_state);
+
+            match self.actions.pop() {
+                Some(action) => action,
+                None => Action::None,
+            }
+        }
+    }
+
+    #[test]
+    fn can_move_test() {
+        assert!(can_move(1, 2));
+        assert!(can_move(2, 3));
+        assert!(can_move(2, 3));
+        assert!(can_move(20, 13));
+
+        assert!(!can_move(20, 1));
+        assert!(!can_move(1, 20));
+        assert!(!can_move(300, 20));
+        assert!(!can_move(1, 200));
+    }
+
+    #[test]
+    fn can_move_and_quit() {
+        let starting_room = 1;
+        // move to room 20 is not possible from room 3.
+        let actions = vec![
+            Action::Quit,
+            Action::Move(20),
+            Action::Move(3),
+            Action::Move(2),
+        ];
+        let expected_states = create_expected_game_states(vec![1, 2, 3, 3]);
+
+        let provider = Box::new(ActionProviderSpy::new(actions, expected_states));
+
+        let mut game = Game::new(starting_room, provider);
+
+        game.run();
+    }
+
+    fn create_expected_game_states(rooms: Vec<RoomNum>) -> Vec<GameState> {
+        let mut result = Vec::new();
+        for (i, room) in rooms.iter().enumerate() {
+            result.push(GameState {
+                turn: i,
+                player_room: *room,
+            });
+        }
+        result
+    }
 }
